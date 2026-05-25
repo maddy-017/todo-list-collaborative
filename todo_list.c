fd57e30870bc08de47e618c9b06fb09f621fc3d2
FarhanShaikh121212 <farhanshaikh121212mm@gmail.com>
2026-05-25 14:18:33 +0530

    Farhan: deleteTask and main function

--- todo_list.c
+++ todo_list.c
@@ -22,3 +22,33 @@ void viewTasks() {
         printf("%d. %s", i + 1, tasks[i]);
     }
 }
+
+void deleteTask() {
+    viewTasks();
+    int num;
+    printf("Enter task number to delete: ");
+    scanf("%d", &num);
+    getchar();
+    for(int i = num - 1; i < count - 1; i++) {
+        strcpy(tasks[i], tasks[i + 1]);
+    }
+    count--;
+    printf("Task deleted!\n");
+}
+
+int main() {
+    int choice;
+    while(1) {
+        printf("\n1. Add Task\n2. View Tasks\n3. Delete Task\n4. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+        getchar();
+        switch(choice) {
+            case 1: addTask(); break;
+            case 2: viewTasks(); break;
+            case 3: deleteTask(); break;
+            case 4: return 0;
+            default: printf("Invalid choice!\n");
+        }
+    }
+}
