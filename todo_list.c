43d6d80b34f090b511b78e25e9af5b764a913ed7
maddy-017 <bangiabdulmoid@gmail.com>
2026-05-25 14:34:47 +0530

    Mueed: addTask and viewTasks

--- todo_list.c
+++ todo_list.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_TASKS 100
-
-char tasks[MAX_TASKS][200];
+char tasks[100][200];
 int count = 0;
 
 void addTask() {
@@ -20,35 +18,5 @@ void viewTasks() {
     }
     for(int i = 0; i < count; i++) {
         printf("%d. %s", i + 1, tasks[i]);
-    }
-}
-
-void deleteTask() {
-    viewTasks();
-    int num;
-    printf("Enter task number to delete: ");
-    scanf("%d", &num);
-    getchar();
-    for(int i = num - 1; i < count - 1; i++) {
-        strcpy(tasks[i], tasks[i + 1]);
-    }
-    count--;
-    printf("Task deleted!\n");
-}
-
-int main() {
-    int choice;
-    while(1) {
-        printf("\n1. Add Task\n2. View Tasks\n3. Delete Task\n4. Exit\n");
-        printf("Enter choice: ");
-        scanf("%d", &choice);
-        getchar();
-        switch(choice) {
-            case 1: addTask(); break;
-            case 2: viewTasks(); break;
-            case 3: deleteTask(); break;
-            case 4: return 0;
-            default: printf("Invalid choice!\n");
-        }
     }
 }
